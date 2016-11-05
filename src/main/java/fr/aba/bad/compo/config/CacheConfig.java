4ae3d6a43a443fe15b46752c10b832dcb8d22cb0
aurelien <aurelien@aurelien-lubuntubase>
2016-11-05 14:13:23 +0100

    [PERF] Enable caching

---
+++ src/main/java/fr/aba/bad/compo/config/CacheConfig.java
@@ -0,0 +1,10 @@
+package fr.aba.bad.compo.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+}
